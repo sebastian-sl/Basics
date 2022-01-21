757b4089a7371f2c90d25a0c827ffdb96fd2f320
sebastian-sl <46265893+sebastian-sl@users.noreply.github.com>
2022-01-21 16:21:27 +0100

    these damn python comments

--- 00 BASICS/ARRAYS/Array.py
+++ 00 BASICS/ARRAYS/Array.py
@@ -8,29 +8,29 @@ myArray = ["Tim", "Julia", "Hans"]
 myArray2 = [5, 8, 2]
 
 # CREATE
-myArray.insert(1, "Lola")							# Insert into specific position (here: 1)
-myArray.append("Peter")								# Append to the end of the array
+myArray.insert(1, "Lola")									# Insert into specific position (here: 1)
+myArray.append("Peter")										# Append to the end of the array
 
 # READ
-print(myArray)								# Prints complete Array
-print(myArray[2])							# Prints the Element with Index
+print(myArray)									# Prints complete Array
+print(myArray[2])								# Prints the Element with Index
 
 # UPDATE
 myArray[0] = "Zoe"								# Updates Element by Index
-myArray2 = list(map(lambda n: n + n, myArray2))			# Maps Function to all Elements
+myArray2 = list(map(lambda n: n + n, myArray2))						# Maps Function to all Elements
 myArray3 = myArray + myArray2							# Joins two Arrays
 
 
 # ITERATE
-for i in range(len(myArray)):						# Iterate by index
+for i in range(len(myArray)):								# Iterate by index
 	print(myArray[i])
 
-for name in myArray:								# Iterate by Element
+for name in myArray:									# Iterate by Element
 	print(name)
 
 # SORT
 myArray.sort()										# Sort ASC - returns iterator
-myArray.sort(reverse=True)							# Sort DESC - returns iterator
+myArray.sort(reverse=True)								# Sort DESC - returns iterator
 
 myArray.reverse()									# Reverses the order of the Array
 
