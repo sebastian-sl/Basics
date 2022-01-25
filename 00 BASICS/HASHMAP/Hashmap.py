0f83a817d814dbb28a42c68570f42e113f06113c
sebastian-sl <46265893+sebastian-sl@users.noreply.github.com>
2022-01-25 19:25:38 +0100

    small typo

--- 00 BASICS/HASHMAP/Hashmap.py
+++ 00 BASICS/HASHMAP/Hashmap.py
@@ -3,7 +3,7 @@
 
 
 # INITIALIZATION		
-employee1 = {									# Best Practice to indent the Code
+employee1 = {							# Best Practice to indent the Code
 	"id": 1,
 	"name": "Peter",
 	"age": 39
@@ -17,31 +17,31 @@ employee2 = {
 
 
 # CREATE
-employee1["gender"] = "m"							# Create a new key value pair in a dictionary
-employee2["gender"] = "f"							# by using the dict[key] = value term
+employee1["gender"] = "m"					# Create a new key value pair in a dictionary
+employee2["gender"] = "f"					# by using the dict[key] = value term
 
 # READ
-print(employee1)								# Prints the whole dictionary
-print(employee1["name"])							# Prints the value of the given key
+print(employee1)						# Prints the whole dictionary
+print(employee1["name"])					# Prints the value of the given key
 
 # UPDATE
-employee1["name"] = "Dieter"							# Update ELement by key
+employee1["name"] = "Dieter"					# Update ELement by key
 
-all_employee = {}								# Merging two dictionaries
-all_employee.update(employee1)							# NOTE: Overrides values with same key!
+all_employee = {}						# Merging two dictionaries
+all_employee.update(employee1)					# NOTE: Overrides values with same key!
 
 
 # ITERATE
-for key in employee1:								# Iterate by Key (same as dict.keys() - can access values aswell)
+for key in employee1:						# Iterate by Key (same as dict.keys() - can access values aswell)
 	print(key, employee1[key])
 
-for key, value in employee1.items():					# Iterate by Element for key & value, more convenient
+for key, value in employee1.items():				# Iterate by Element for key & value, more convenient
 	print(key, value)
 
 
 # DELETE
-employee1.pop("age")								# removes Item by Key
-employee1.popitem()								# removes last item in the dictionary
+employee1.pop("age")						# removes Item by Key
+employee1.popitem()						# removes last item in the dictionary
 
 
 all_employee.clear()							# clears the whole dictionary
