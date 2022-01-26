155ba6e0c7a7ca0d7bc4e65bcaf529a6c9dcc2a6
sebastian-sl <46265893+sebastian-sl@users.noreply.github.com>
2022-01-26 13:13:27 +0100

    added javascript map and normalized python

---
+++ 00 BASICS/HASHMAP/Hashmap.js
@@ -0,0 +1,16 @@
+/* 	In Javascript there are multiple ways to create a hashmap/dictionary
+	like the standard Object notation but since ES6 we can use the Map
+	object which i will use in the following code.
+	(Source: Mozilla Developer Network) */
+
+
+
+// INITIALIZATION
+const employee1 = new Map(
+	["id", 1],
+	["name", "Peter"],
+	["age": 39]
+	)
+
+
+console.log(employee1)
--- 00 BASICS/HASHMAP/Hashmap.py
+++ 00 BASICS/HASHMAP/Hashmap.py
@@ -25,11 +25,14 @@ print(employee1["name"])					# Prints the value of the given key
 # UPDATE
 employee1["name"] = "Dieter"					# Update ELement by key
 
-all_employee = {}						# Merging two dictionaries
-all_employee.update(employee1)					# NOTE: Overrides values with same key!
-
 # ITERATE
-for key in employee1:						# Iterate by Key (same as dict.keys() - can access values aswell)
+for key in employee1.keys():						# Iterate over the key only
+	print(key)
+
+for value in employee1.values():					# Iterate over the values only
+	print(value)
+
+for key in employee1:								# Iterate over the entries
 	print(key, employee1[key])
 
 for key, value in employee1.items():				# Iterate by Element for key & value, more convenient
@@ -37,5 +40,4 @@ for key, value in employee1.items():				# Iterate by Element for key & value, mo
 
 # DELETE
 employee1.pop("age")						# removes Item by Key
-employee1.popitem()						# removes last item in the dictionary
-all_employee.clear()						# clears the whole dictionary
+employee2.clear()						# clears the whole dictionary
