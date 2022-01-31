aafff5b4295c4d9e54f4482555954ba3833203fd
sebastian-sl <46265893+sebastian-sl@users.noreply.github.com>
2022-01-31 20:57:21 +0100

    Create String.js

---
+++ 00 BASICS/STRINGS/String.js
@@ -0,0 +1,38 @@
+// Strings
+
+// CREATE
+let mystring = "Hello World, this is cool"
+let mystring1 = "ok lets start!"
+
+// READ
+console.log(mystring)
+
+x = mystring.slice(1,2)                         // gets the 2nd index. Slice(start, end)
+x = mystring.slice(0, 2)                        // gets everything until the 2nd index
+x = mystring.slice(2)                           // gets everything from the 2nd index to end -> same as slice(2, )
+x = mystring.slice(3, 5)                        // gets everything from 3rd to 5th index (5th not included!)
+x = mystring.slice(-4, -2)                      // same rules apply but backwards, everything from 5th last to 2nd last
+
+// UPDATE (Strings are immutable, so creating new objects)
+mystring1 = mystring1.replace("k", "kay")       // replaces first with second argument
+
+mystring = mystring.toUpperCase()                           // all uppercase
+mystring = mystring.toLowerCase()                           // all lowercase
+mystring = mystring[0].toUpperCase() + mystring.slice(1)    // sadly there is no method for that so we need to use two of the presented methods
+
+mystring = mystring.trim()                      // removes leading and trailing whitespaces
+
+mystring_list = mystring.split()                // splits the string into a list, separates by given argument
+
+// ITERATE
+for (let i = 0; i < mystring.length; i++) {    // iterate by index
+    console.log(mystring[i])
+}
+
+for (let char of mystring) {                   // iterate by element
+    console.log(char)
+}
+
+// F String
+x = `${mystring}! ${mystring1}`                // new ES6 way of creating template string. note the `` not quotes ''
+x = mystring + "! " + mystring1                // old way, still works too :)
