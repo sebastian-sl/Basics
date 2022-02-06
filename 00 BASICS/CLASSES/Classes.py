1276eb6a9fe66b089fb1c1a071cd45c65f758d90
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-02-06 14:09:12 +0100

    Update Classes.py

--- 00 BASICS/CLASSES/Classes.py
+++ 00 BASICS/CLASSES/Classes.py
@@ -37,7 +37,7 @@ class department(Enum):                    # set of symbolic names bound to unqi
     MARKETING = 3
 
 
-# INHHERITANCE examples
+# INHHERITANCE example
 class HR_Employee(Employee):                        # Without init, subclass inherits the init from parents! Super() 'manually'
     def __init__(self, fname, lname, division):     # inherits for given arguments. We do this to add another attr!
         super().__init__(fname, lname)
