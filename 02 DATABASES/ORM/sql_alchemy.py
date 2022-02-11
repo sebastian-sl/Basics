e881467130b93b584061458d99486b4e6c2ab3d9
sebastian-sl <46265893+sebastian-sl@users.noreply.github.com>
2022-02-11 23:43:10 +0100

    Update sql_alchemy.py

--- 02 DATABASES/ORM/sql_alchemy.py
+++ 02 DATABASES/ORM/sql_alchemy.py
@@ -2,17 +2,14 @@ import sqlalchemy as db
 import sqlalchemy.ext.declarative as dbd
 import sqlalchemy.orm as dbo
 
-# Connection string as in normal db connection (here ms access)
 con_str = (
     r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-    r"DBQ=C:testdb.accdb;"
+    r"DBQ=C:\Users\Seb\Desktop\Software Dev\04 Data\testdb.accdb;"
     )
 
-# Creating engine and declarative base
 engine = db.create_engine(f"access+pyodbc:///?odbc_connect={con_str}")
 Base = dbd.declarative_base()
 
-# Declaring classes which will (or are already) records in the database with instance attributes as columns
 class User(Base):
     __tablename__ = "Tabelle1"
     ID = db.Column("ID", db.Integer)
@@ -25,7 +22,7 @@ class Person(Base):
     vorname = db.Column(db.String, primary_key=True)
     nachname = db.Column(db.String, nullable=True)
 
-# After declaring classes we create metdata (tablenames above will be created if they don't exist - existing wont be overwritten)
+# After declaring all Classes (so new tables will be created - existing tables wont be overwritten)
 Base.metadata.create_all(engine)
 
 # Session
@@ -33,9 +30,17 @@ session = dbo.sessionmaker()
 session.configure(bind=engine)
 s = session()
 
-# Running a basic query (select)
+
+# Select all
 for user in s.query(User).all():
     print(user.alter)
 
-names = [user.nachname for user in s.query(User).all()]
-print(names)
+# Select with conditions
+for user in s.query(User).filter(User.alter > 15):
+    print(user.alter, user.fname)
+
+# Creating new Instance and adding it to the db
+peter = User(ID=5, fname="Peter", nachname="Giesel", alter=59)
+s.add(peter)
+
+s.commit()
