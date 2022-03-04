05e13e96f66d727a928b7b6e93d6916de3f99287
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:03:45 +0100

    Update Array.js

--- 00 BASICS/ARRAYS/Array.js
+++ 00 BASICS/ARRAYS/Array.js
@@ -4,36 +4,36 @@
 myArray = ["Tim", "Julia", "Hans"];
 myArray2 = [5, 8, 2];
 
+// READ
+console.log(myArray);                           // complete Array
+console.log(myArray[1])                         // elements
+
 // CREATE
 myArray.push("Lola");                           // Append to the end of the Array
-myArray.splice(1, 0, "Peter");                  // Insert into Position, Delete Count, Element
-
-// READ
-console.log(myArray);                           // Prints complete Array
-console.log(myArray[1])                         // Prints the Element with Index
+myArray.splice(1, 0, "Peter");                  // Insert into (Position, Delete Count, Element)
 
 // UPDATE
 myArray[0] = "Zoe"                              // Updates Element by Index
-myArray2 = myArray2.map((e) => e+e)             // Maps function to all Elements with Arrow (forEach works aswell but doesn't create new array)
-myArray3 = myArray.concat(myArray2)             // Joins two Arrays
 
-// ITERATE
+// ITERATATION BY INDEX
 for (let i = 0; i < myArray.length; i++) {      // Iterate by index
     console.log(myArray[i]);
 }
 
+// ITERATION BY ELEMENT
 for (let name of myArray) {                     // Iterate by Element
     console.log(name);
 }
 
 // SORT
-myArray.sort()                                  // SORT ASC
-myArray.sort().reverse()                        // SORT DESC
+myArray.sort()                                  // ASC, myArray.sort().reverse() for desc
 
+// REVERSE
 myArray.reverse()                               // Reverses the order of the Array
 
-// DELETE
-myArray.pop()                       // Removes last Element
-myArray.filter(e => e !== "Julia")  // Remove by Value with Arrow Function
-myArray.splice(3, 1)                // Remove by Index, Delete Count
-myArray.length = 0                  // Removes everything
+// DELETE element
+myArray.filter(e => e !== "Julia")              // Remove by Value with Arrow Function
+myArray.splice(3, 1)                            // Remove by Index, Delete Count
+
+// DELETE all
+myArray.length = 0                              // Removes everything
