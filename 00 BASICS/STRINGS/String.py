649aaf5cc1b28d5fca0e0ca23e77f7cedfedeee1
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:28:33 +0100

    Update String.py

--- 00 BASICS/STRINGS/String.py
+++ 00 BASICS/STRINGS/String.py
@@ -1,36 +1,32 @@
-# Strings
-
-
 # CREATE
 mystring = "Hello World"
 mystring1 = "Ok lets start!"
 
-# READ
+# READ all
 print(mystring)                             # prints the whole String
 
-x = mystring[2]                             # gets only the 2nd character of the string (remember index starts at 0)
-x = mystring[:2]                            # gets everything (:) until the 2nd index
-x = mystring[2:]                            # gets everything from the 2nd index to the end (:)
-x = mystring[3:5]                           # gets everything from 3rd to 5th index (5th not included)
-x = mystring[-5:-2]                         # same rules apply but backwards, everything from 5th last to 2nd last
+# READ substring
+x = mystring[2]                             # gets only the 2nd character of the string
+x = mystring[:2]                            # gets everything until the 2nd index (2: for everything from 2nd to end)
+x = mystring[-5:-2]                         # backwards, everything from 5th last to 2nd last
 
-# UPDATE (Strings are immutable, so we need to create a new object)
-mystring1 = mystring1.replace("k", "kay")   # replaces first with second argument
+# UPDATE
+mystring1 = mystring1.replace("k", "kay")   # replaces first with second argument (need to create new string because strings are immutable)
 
-mystring = mystring.upper()                 # all uppercase
-mystring = mystring.lower()                 # all lowercase
+mystring = mystring.upper()                 # all uppercase, mystring.lower() for lowercase
 mystring = mystring.capitalize()            # capitalizing
 
 mystring = mystring.strip()                 # removes leading and trailing whitespaces
 
 mystring_list = mystring.split(" ")         # Splits the String into a list, separates by given argument
 
-# ITERATE
-for i in range(len(mystring)):              # iterate by index with len(string)
+# ITERATE by Index
+for i in range(len(mystring)):
     print(i)
 
-for char in mystring:                       # iteration by element
+# ITERATE by Element
+for char in mystring:
     print(char)
 
-# FORMAT STRING / CONCAT
+# TEMPLATE STRING
 x = f"{mystring}, {mystring1}"
