14a54a8d2556645b1b74671241cbe4a1188d8d1c
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:43:28 +0100

    Update Classes.js

--- 00 BASICS/CLASSES/Classes.js
+++ 00 BASICS/CLASSES/Classes.js
@@ -25,24 +25,12 @@ class Engineer {
 console.log(Engineer.company)          // CLASS attr
 Engineer.intro()                       // CLASS method
 
-
-// ENUM example
-const department = {                  // ENUM work as class attr
-    HR: "HR",
-    Finance: "Finance",
-    Marketing: "Marketing"
-}
-
-
 // INHERITANCE example
 class HREmployee extends Employee {
     constructor(fname, lname, division) {
         super(fname, lname);
         this.division = division;
     }
-
-    department = department.HR;
 }
 let obj1 = new HREmployee("Zoe", "Miller", "Hiring")    // creating SUBCLASS INSTANCE
-console.log(obj1.department)                            // SUBCLASS attr
 obj1.intro()                                            // PARENTS INSTANCE method
