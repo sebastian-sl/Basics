578df84c86eabe983709ed756bbc48b9be307f78
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:06:36 +0100

    Update Array.py

--- 00 BASICS/ARRAYS/Array.py
+++ 00 BASICS/ARRAYS/Array.py
@@ -1,38 +1,34 @@
-# PYTHON ARRAYS
-
 # INITIALIZATION
 myArray = ["Tim", "Julia", "Hans"]
-myArray2 = [5, 8, 2]
-
-# CREATE
-myArray.insert(1, "Lola")								# Insert into specific position (here: 1)
-myArray.append("Peter")									# Append to the end of the array
 
 # READ
-print(myArray)										# Prints complete Array
-print(myArray[2])									# Prints the Element with Index
+print(myArray)										# complete Array
+print(myArray[2])									# element
+
+# CREATE
+myArray.insert(1, "Lola")								# Insert into specific position
+myArray.append("Peter")									# Append to the end of the array
 
 # UPDATE
 myArray[0] = "Zoe"									# Updates Element by Index
-myArray2 = list(map(lambda n: n + n, myArray2))						# Maps Function to all Elements
-myArray3 = myArray + myArray2								# Joins two Arrays
 
-
-# ITERATE
-for i in range(len(myArray)):								# Iterate by index
+# ITERATE BY INDEX
+for i in range(len(myArray)):								
 	print(myArray[i])
-
-for name in myArray:									# Iterate by Element
+	
+# ITERATE BY ELEMENT
+for name in myArray:									
 	print(name)
 
 # SORT
-myArray.sort()										# Sort ASC - returns iterator
-myArray.sort(reverse=True)								# Sort DESC - returns iterator
+myArray.sort()										# ASC, myArray.sort(reverse=True) for desc
 
-myArray.reverse()									# Reverses the order of the Array
+# REVERSE
+myArray.reverse()
 
-# DELETE
-myArray.pop()										# Removes last Element
+# DELETE element
 myArray.remove("Julia")									# Remove Element by Value
 del myArray[2]										# Remove Element by Index
+
+# DELETE all
 myArray.clear()										# Removes everything
