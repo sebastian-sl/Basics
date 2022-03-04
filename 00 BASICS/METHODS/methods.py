8e3e616aebc8b2f1c1680075a3df87ad8364e727
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:21:58 +0100

    Update methods.py

--- 00 BASICS/METHODS/methods.py
+++ 00 BASICS/METHODS/methods.py
@@ -1,36 +1,29 @@
-# Methods
-
-mystring = "teststring"
-myint = 5
-myarray = [5, 6, 7]
-
-# FUNCTIONS
-def myfunction1():                       # Basic function, can be executed with myfunction1(). Returns the value, so it can be passed!
+# BASIC FUNCTIONS
+def myfunction1():                       # myfunction1() to execute
     return "Hello World"
 
-def myfunction2(number=1):             # with argument an argument, equals means default value if no argument given
+# FUNCTIONS WITH ARGUMENT
+def myfunction2(number=1):               # equal means default value when none is given 
     return number * number
 
-def myfunction3(*args):                  # with as many arguments as we want, need to iterate over the result in the function
+# FUNCTIONS WITH UNLIMITED ARGUMENTS
+def myfunction3(*args):                  # need to iterate over the arguments
     for arg in args:
         print(arg)
 
-def myfunction4(**kwargs):               # with key-value pairs as arguments, need to iterate over as in a dict
-    for key, value in kwargs.items():   # arguments will be passed like this: myfunction(id=1, name=Peter).
-        print(key, value)               # You cant pass a dict as kwargs!
+# FUNCTIONS WITH KEY-VALUE PAIRS        # arguments will be passed like this: myfunction(id=1, name=Peter)
+def myfunction4(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
 
 
-# BUILT-IN METHODS
-myinput = input("Please input a number: ")      # gets input of user! Note: will be a string, so remember converting
-
+# LAMBDA FUNCTIONS
 func = lambda a,b: a * b                          # lambda/anonymous function
-
-myarray1 = list(map(myfunction2, myarray))         # maps a function to an iterable, returns map object so we convert to list()
 myarray1 = list(map(lambda x: x*x, myarray))       # lambda and mapping together
 
-x = ord("A")                                        # returns the unicode code point value of given character (here: 65)
-x = chr(65)                                         # returns the string of the unicode code point (here: "A")
+# SIMULTANEOUS ITERATION
+myarray = [5, 6, 7]
+myarray1 = [25, 36, 49]
 
-result = list(zip(myarray, myarray1))               # Parallel iteration over two iterables
-for num, square in zip(myarray, myarray1):          # returns a zip object, so needs to be converted
-    print(num, square)                              # here: 5 25, 6 36, 7 49
+for num, square in zip(myarray, myarray1):
+    print(num, square)
