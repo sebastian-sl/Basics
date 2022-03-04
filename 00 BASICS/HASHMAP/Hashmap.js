b45687c943b39ad87d4f1362af5338ecdf15bc67
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:38:04 +0100

    Update Hashmap.js

--- 00 BASICS/HASHMAP/Hashmap.js
+++ 00 BASICS/HASHMAP/Hashmap.js
@@ -1,40 +1,34 @@
 /* 	In Javascript there are multiple ways to create a hashmap/dictionary like the standard Object notation but since 
 	ES6 we can use the Map object which i will use in the following code. (Source: Mozilla Developer Network) */
 
-
-// INITIALIZATION (note the array of key-value pairs)
+// INITIALIZATION
 const employee1 = new Map([                    
 	["id", 1],
 	["name", "Peter"],
 	["age", 39]
 ])
 
-const employee2 = new Map([
-    ["id", 2],
-    ["name", "Peter"],
-    ["age", 27]
-])
+// READ
+console.log(employee1)                      // complete map
+console.log(employee1.get("name"))          // value by given key
 
 // CREATE
-employee1.set("gender", "m")
-employee2.set("gender", "m")
-
-// READ
-console.log(employee1)                      // prints the whole map
-console.log(employee1.get("name"))          // print the value of given key
+employee1.set("gender", "m")		    // new key value pair (if key doesn't exist)
 
 // UPDATE
-employee1.set("name", "Dieter")             // Update value by key (if key doesnt' exist, will be created)
+employee1.set("name", "Dieter")
 
-// ITERATE
+// ITERATE over keys
 for (let key of employee1.keys()) {         // Iterate over the key only
     console.log(key);
 }
 
+// ITERATE over values
 for (let value of employee1.values()) {     // Iterate over the values only
     console.log(value);
 }
 
+// ITERATE over elements
 for (let entry of employee1) {              // Iterates over the key-value pairs (as array! 0 is key, 1 is value)
     console.log(entry);
     console.log(entry[0]);
