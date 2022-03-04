efa5dcccfe07dfe7422568483ab437a4b5b5c9e8
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-04 23:59:03 +0100

    Update Array.java

--- 00 BASICS/ARRAYS/Array.java
+++ 00 BASICS/ARRAYS/Array.java
@@ -1,13 +1,14 @@
 /*  Since Java (in contrast to Python/JS) differentiates between lists and array, i will show both ways (Array first, then List). Main Differences:
     -> Arrays have fixed size and are therefore better performance wise
-    -> Lists are more flexible and have bit more utility                */
+    -> Lists are more flexible and have bit more utility                                                                                        */
+
 import java.util.*;
 
 public class Array {
   public static void main(String[] args) {
 
             // DECLARATION
-            String[] exampleArray = new String[3];                  // need to give size
+            String[] exampleArray = new String[3];                  // need to give size of array
             List<String> exampleList = new ArrayList<>();
 
             // INITIALIZE
@@ -23,7 +24,7 @@ public class Array {
             System.out.println(myList.get(0));
 
             // CREATE
-            exampleArray[0] = "Peter";                          // You can't add a new element to an array, because of the fixed size! (see variable name)
+            exampleArray[0] = "Peter";                          // can't add new items to an array
             myList.add("Peter");
 
             // UPDATE
@@ -31,7 +32,7 @@ public class Array {
             myList.set(0, "Julia");
 
             // ITERATION BY INDEX
-            for (int i = 0; i < myArray.length; i++) {          // For List: myList.size()
+            for (int i = 0; i < myArray.length; i++) {          // For List: i < myList.size()
                 System.out.println(myArray[i]);
                 System.out.println(myList.get(i));
             }
@@ -42,8 +43,8 @@ public class Array {
             }
 
             // SORT
-            Arrays.sort(myArray);                              // ASC, sort(myArray, Collections.reverseOrder()) for DESC
-            Collections.sort(myList);                          // ASC, sort(myList, Collections.reverseOrder()) for DESC
+            Arrays.sort(myArray);                              // ASC, sort(myArray, Collections.reverseOrder()) for desc
+            Collections.sort(myList);                          // ASC, sort(myList, Collections.reverseOrder()) for desc
 
             // REVERSE
             Collections.reverse(Arrays.asList(myArray));        // reversing the order
@@ -54,7 +55,7 @@ public class Array {
             myList.remove(0);
 
             // DELETE all
-            myArray = null;
+            myArray = null;                                     // can't completly delete an array
             myList.clear();
     }
 }
