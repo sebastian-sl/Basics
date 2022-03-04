608758c99cf6f5a0e39caf49c94cb334a0902aa8
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-03-05 00:41:30 +0100

    Update Hashmap.java

--- 00 BASICS/HASHMAP/Hashmap.java
+++ 00 BASICS/HASHMAP/Hashmap.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 /*  Java is way more strict with hashmaps compared to Python/Javascript. A Hashmap in Java con only hold key-value pairs with the 
-every key being the same dataype as all other keys, same goes for the values. Will discuss two possible alternatives at the bottom.
+every key being the same dataype as all other keys, same goes for the values. Will discuss two possible alternatives at the bottom. 
 */
+import java.util.*;
 
 public class Hashmap {
     public static void main(String[] args) {
+        
         // DECLARATION
         HashMap<String, String> user = new HashMap<>();
 
@@ -17,22 +15,23 @@ public class Hashmap {
             2, "Julia",
             3, "Monika"
         ));
+        
+        // READ
+        System.out.println(employees);              // complete Hashmap
+        System.out.println(employees.get(3));       // value by given key
 
         // CREATE
-        employees.put(4, "Maria");
-
-        // READ
-        System.out.println(employees);              // Prints the whole Hashmap
-        System.out.println(employees.get(3));       // Prints Value by given Key
+        employees.put(4, "Maria");                  // new key value pair (if key doesn't exist otherwise updated)
 
         // UPDATE
-        employees.put(4, "Tim");                    // Update Value by Key (created if it doesn't exist)
+        employees.put(4, "Tim");
 
-        // ITERATE
-        for (Integer key: employees.keySet()) {     // Iterates over the keys only (can access values aswell)
+        // ITERATE over keys
+        for (Integer key: employees.keySet()) {
             System.out.println(key + ", " + employees.get(key));
         }
 
+        // ITERATE over values
         for (String value: employees.values()) {    // Iterates over the values only
             System.out.println(value);
         }
@@ -43,10 +42,8 @@ public class Hashmap {
 
 
         /* Alternatives for different Datatypes:
-        1)  Using Type Object, but this creates a rat tail since you can only access the values as Type of Objects.
-            So if we want to continue working with the values, we would have to continue with Object type (i.e. List<Object>)
-
-        2)  Using a class instead 
-        */
+        1)  Using Type Object, but this creates a rat tail since you can only access the values as Type of Objects. So if we want
+            to continue working with the values, we would have to continue with the Object type all the time (for example: List<Object>)
+        2)  Using a class instead                                                                                                     */
     }
 }
