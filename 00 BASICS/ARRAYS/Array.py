1e8587f456bb4bb87576a4a4a5403b364c200a84
elnino <46265893+sebastian-sl@users.noreply.github.com>
2022-08-21 21:33:37 +0200

    Update Array.py

--- 00 BASICS/ARRAYS/Array.py
+++ 00 BASICS/ARRAYS/Array.py
@@ -2,15 +2,15 @@
 myArray = ["Tim", "Julia", "Hans"]
 
 # READ
-print(myArray)										# complete Array
-print(myArray[2])									# element
+print(myArray)										# print complete Array
+print(myArray[2])									# print one element of the Array (2nd index)
 
 # CREATE
 myArray.insert(1, "Lola")								# Insert into specific position
 myArray.append("Peter")									# Append to the end of the array
 
 # UPDATE
-myArray[0] = "Zoe"									# Updates Element by Index
+myArray[0] = "Zoe"									# Updates Value of an Element of the Array
 
 # ITERATE BY INDEX
 for i in range(len(myArray)):								
