18badfa9358bcd6e4b26d1df2af4b2b25b16284a
Sebastian <yallahassan123@web.de>
2022-11-24 21:05:19 +0100

    added template for flask

--- 08 BACKEND/FLASK/flask_template.py
+++ 08 BACKEND/FLASK/flask_template.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "")
 
 # return String
 @app.route("/", methods=["GET"])
 def main():
     return "HELLO WORLD"
 
+
+# returns HTML webpage with parameter as props (can be accessed by {{}} within HTML)
+@app.route("/template", methods=["GET"])
+def template():
+    message = "HELLO WORLD"
+    return render_template("index.html", parameter = message)
 
 # return Json Object
 @app.route("/<parameter>", methods=["GET"])
@@ -18,6 +24,22 @@ def api(parameter):
         "City": "Chicago",
         "Country": "United States"
     })
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
+
+
+
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__, template_folder="..\\html")
+
+@app.route("/", methods=["GET"])
+def main():
+    return render_template("off.html", message="This is coming from Jinja2!", message_two="KANYE FOR PRESIDENT")
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
